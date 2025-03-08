b0885f493a0806d9d929fbb4b5f1e4eb9a4baedf
Tuân <tuan.tl.pham@protonmail.com>
2025-03-08 17:47:41 +0100

    Création de l'oiseau et de son mouvement
    J'ai ajouté le mouvement à l'oiseau, et j'ai aussi ajouté l"horloge" pygame pour limiter les fps (sinon la vitesse d'éxécution du jeu dépendait de la performance de l'ordi)

--- src/main.py
+++ src/main.py
@@ -5,23 +5,53 @@ WIDTH, HEIGHT = BG_IMAGE.get_width(), BG_IMAGE.get_height()
 
 BIRD_IMAGE = pygame.image.load("assets/images/bird.png")
 
+class Bird:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.gravity = 0.5 # Force de la gravité
+        self.y_velocity = 0
+        self.jump_force = 10 # Force du saut de l'oiseau
+
+    def update(self):
+        if self.y_velocity > -10: # Limite de vitesse max
+            self.y_velocity -= self.gravity
+        self.y -= self.y_velocity
+
+    def draw(self, screen):
+        screen.blit(BIRD_IMAGE, (self.x, self.y))
+        
+    def jump(self):
+        self.y_velocity = self.jump_force
+
 class Game:
     def __init__(self):
+        pygame.init()
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         pygame.display.set_caption("Flappy Bird de chez Wish")
+        self.bird = Bird(50, HEIGHT//2)
 
     def draw_screen(self):
         self.screen.blit(BG_IMAGE, (0, 0))
-        self.screen.blit(BIRD_IMAGE, (50, 150))
-
+        self.bird.draw(self.screen)
+    
     def run(self):
-        while True:
+        clock = pygame.time.Clock()
+        running = True
+        while running:
+            clock.tick(60) # Limiter les FPS à 60 par secondes 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    pygame.quit()
-                    quit()
+                    running = False
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE or event.key == pygame.K_UP:
+                        self.bird.jump()
+                        
+            self.bird.update()
             self.draw_screen()
             pygame.display.update()
+        
+        pygame.quit()
 
 def main():
     game = Game()
